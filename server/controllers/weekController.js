57b63feb8d3a6000b8784972038b392870fc2580
SamirElKharrat <71700525+SamirElKharrat@users.noreply.github.com>
2025-10-07 18:56:25 +0100

    Fix
    Cambio de fechas para predecir

--- server/controllers/weekController.js
+++ server/controllers/weekController.js
@@ -1,20 +1,21 @@
 const startOfWeek = (date = new Date()) => {
     const day = date.getDay();
-    const wedOffset = day >= 3 ? day - 3 : 4 + day;
+    const offset = (day + 6) % 7;
     const startOfWeek = new Date(date);
-    startOfWeek.setDate(date.getDate() - wedOffset);
+    startOfWeek.setDate(date.getDate() - offset);
     startOfWeek.setHours(0, 0, 0, 0);
     return startOfWeek;
-}
+};
 
 const endOfWeek = (date = new Date()) => {
     const day = date.getDay();
-    const wedOffset = day >= 3 ? day - 3 : 4 + day;
+    const offset = (day + 6) % 7;
     const endOfWeek = new Date(date);
-    endOfWeek.setDate(date.getDate() + 6 - wedOffset);
+    endOfWeek.setDate(date.getDate() + 7 - offset);
     endOfWeek.setHours(23, 59, 59, 999);
     return endOfWeek;
-}
+};
+
 
 module.exports = {
     startOfWeek,
